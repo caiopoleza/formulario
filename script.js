caff620e2ef3ed21cd037239743f6be35d894e04
caiopoleza <caio_p_santos@estudante.sesisenai.org.br>
2025-04-15 12:58:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function validarformulario() {
+
+    const nome = document.getElementById('nome_pessoa').Value.trim();
+
+
+
+
+
+    if (!nome) {
+
+        alert('coloque o nome');
+        return;
+
+    } else{ 
+  console.log(nome);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
